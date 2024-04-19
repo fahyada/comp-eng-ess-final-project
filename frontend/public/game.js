e9ff20491500e01fc411927027bdfd74e237a265
fahyada <fahyada@gmail.com>
2024-04-19 21:48:40 +0700

    i

--- frontend/public/game.js
+++ frontend/public/game.js
@@ -6,7 +6,6 @@ let playerName = "";
 
 function generateId() {
     let playerId = Math.floor(Math.random() * 1000000); // Generate a random ID between 0 and 999999
-    document.getElementById("playerId").value = playerId; // Set the generated ID to the playerId input field
     document.getElementById("generatedIdDisplay").innerText = "Your ID: " + playerId; // Display the generated ID to the user
 
 }
